6e9cea45a1cd4cdc0458705628c9b8fe44b51649
Brett Birth <brett.birth@gmail.com>
2016-08-18 09:44:42 -0500

    Add error handling for user registration

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,12 @@
 class WelcomeController < ApplicationController
+  before_action :authenticate_user!
+
   def index
   	@user = current_user
   	@heroes = Hero.all
   	url = "https://api.lootbox.eu/#{@user.platform}/#{@user.region}/#{@user.tag}/profile"
   	response = RestClient.get(url)
   	@data = JSON.parse(response)
-  	
+
   end
 end
