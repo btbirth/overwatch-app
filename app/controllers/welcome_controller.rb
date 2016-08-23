4efe55f4339a73f239784f1ac7938a4f0f4bc047
Brett Birth <brett.birth@gmail.com>
2016-08-23 08:30:50 -0500

    takes out the use of the no longer working api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,9 +4,18 @@ class WelcomeController < ApplicationController
   def index
   	@user = current_user
   	@heroes = Hero.all
-  	url = "https://api.lootbox.eu/#{@user.platform}/#{@user.region}/#{@user.tag}/profile"
-  	response = RestClient.get(url)
-  	@data = JSON.parse(response)
+ 	# url = "https://api.lootbox.eu/#{@user.platform}/#{@user.region}/#{@user.tag}/profile"
+  	
+  # 	url2 = "https://api.lootbox.eu/psn/us/NecromorphSteve/competitive-play/hero/"
+  # 	@heroes.each do |hero|
+  # 		url2 = url2 + hero.name + "%2C"
+  # 	end
+  # 	url2.chomp!("%2C")
 
+  	# response = RestClient.get(url)
+  	# @data = JSON.parse(response)
+  
+
+ 
   end
 end
